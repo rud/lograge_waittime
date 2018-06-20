82b89a63b48c1c2fb728ccbd638c2de4a14a216b
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-20 14:09:01 +0200

    RequestStartedMiddleware: do nothing but be installed just yet

---
+++ test/lib/lograge_rails_request_queueing/request_started_middleware_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+module LogrageRailsRequestQueuing
+  class RequestStartedMiddlewareTest < ActionDispatch::IntegrationTest
+    test 'middleware is installed' do
+      assert Rails.application.middleware.include?(LogrageRailsRequestQueuing::RequestStartedMiddleware)
+    end
+  end
+end
