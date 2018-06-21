7b601c5b12366c3ae9be87ebee22c6a72587012b
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-21 12:56:12 +0200

    Extract queue_time_ms

--- test/dummy/app/controllers/echos_controller.rb
+++ test/dummy/app/controllers/echos_controller.rb
@@ -4,14 +4,14 @@ class EchosController < ApplicationController
     if nginx_request_start.present? && nginx_request_start =~ /t=([\.\d+]+)/
       timestamp = Float($1)
       queue_time_seconds = RequestStore[:lograge_request_handling_started].to_f - timestamp
+      queue_time_ms = (queue_time_seconds * 1000).round(2)
     end
-
 
     render json: {
       lograge_request_handling_started: RequestStore[:lograge_request_handling_started],
       lograge_request_handling_started_f: RequestStore[:lograge_request_handling_started].to_f.to_s,
       HTTP_X_REQUEST_START: nginx_request_start,
-      queue_time_ms: (queue_time_seconds * 1000).round(2)
+      queue_time_ms: queue_time_ms
     }
   end
 
