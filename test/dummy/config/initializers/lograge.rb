78f2507642bd4b2f33dbcf9bb55dd4fc20b8331b
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-21 13:49:51 +0200

    lograge.custom_options: return nil after setting event settings

--- test/dummy/config/initializers/lograge.rb
+++ test/dummy/config/initializers/lograge.rb
@@ -9,6 +9,6 @@ Rails.application.configure do
   config.lograge.custom_options = lambda do |event|
     queued_ms = RequestStore[:lograge_request_queueing].queued_ms
     event[:rq] = queued_ms.round(2) if queued_ms
-    event
+    nil
   end
 end
