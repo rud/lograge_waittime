e5a5cc49afc88c761c5229b92e7b3a73d88f0dca
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-21 14:17:46 +0200

    docker-compose: activate rails logging to stdout in production

--- test/dummy/config/initializers/lograge.rb
+++ test/dummy/config/initializers/lograge.rb
@@ -1,10 +1,8 @@
 Rails.application.configure do
-  config.logger = ActiveSupport::Logger.new(STDOUT)
-  
   config.lograge.enabled = true
 
   # Keep emitting the verbose logging for easier debug
-  config.lograge.keep_original_rails_log = true
+  config.lograge.keep_original_rails_log = !Rails.env.production?
 
   config.lograge.custom_options = lambda do |event|
     queued_ms = RequestStore[:lograge_request_queueing].queued_ms
