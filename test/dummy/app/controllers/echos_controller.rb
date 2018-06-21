c8d2f23d1383128ac1f3740617009051b5be02fa
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-21 12:37:46 +0200

    nginx: add X-Request-Start header line and echo the value (works!)
    To test: in one terminal do:
        docker-compose up
    
    In another terminal:
        curl -s localhost:3030/echo | jq
        {
          "lograge_request_handling_started": "2018-06-21T10:37:23.628+00:00",
          "X_REQUEST_START": "t=1529577443.533"
        }
    
    Yay

--- test/dummy/app/controllers/echos_controller.rb
+++ test/dummy/app/controllers/echos_controller.rb
@@ -2,8 +2,7 @@ class EchosController < ApplicationController
   def index
     render json: {
       lograge_request_handling_started: RequestStore[:lograge_request_handling_started],
-      X_REQUEST_START: request.env['X-Request-Start'],
-      env_vars: request.env.keys
+      X_REQUEST_START: request.env['HTTP_X_REQUEST_START']
     }
   end
 
