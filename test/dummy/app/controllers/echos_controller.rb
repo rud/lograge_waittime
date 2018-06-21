b47fee96d332c4ed27d71d6dd36716e684c2a6af
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-21 12:39:32 +0200

    Adjust output variable name to match request header

--- test/dummy/app/controllers/echos_controller.rb
+++ test/dummy/app/controllers/echos_controller.rb
@@ -2,7 +2,7 @@ class EchosController < ApplicationController
   def index
     render json: {
       lograge_request_handling_started: RequestStore[:lograge_request_handling_started],
-      X_REQUEST_START: request.env['HTTP_X_REQUEST_START']
+      HTTP_X_REQUEST_START: request.env['HTTP_X_REQUEST_START']
     }
   end
 
