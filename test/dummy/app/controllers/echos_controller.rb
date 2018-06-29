e79c3cc0d307f546a1cc024d7f26b928a9b5bbbf
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-29 14:11:36 +0200

    Echo: output the request_queued_raw value in the response

--- test/dummy/app/controllers/echos_controller.rb
+++ test/dummy/app/controllers/echos_controller.rb
@@ -1,6 +1,7 @@
 class EchosController < ApplicationController
   def index
     render json: {
+      request_queued_raw: RequestStore[:lograge_request_queueing].request_queued_raw,
       queued_ms: RequestStore[:lograge_request_queueing].queued_ms
     }
   end
