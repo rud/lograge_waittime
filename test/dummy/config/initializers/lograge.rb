3342013bf9cd62d0ace8a07453c8aa0c90f4b28a
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-29 20:37:52 +0200

    Dummy app: log exceptions in structured format

--- test/dummy/config/initializers/lograge.rb
+++ test/dummy/config/initializers/lograge.rb
@@ -4,12 +4,20 @@ Rails.application.configure do
   # Keep emitting the verbose logging for easier debug
   config.lograge.keep_original_rails_log = !Rails.env.production?
 
-  config.lograge.custom_options = lambda do |_event|
+  config.lograge.custom_options = lambda do |event|
     custom_options = {}
 
     queued_ms = RequestStore[:lograge_request_queueing].queued_ms
     custom_options[:rq] = queued_ms.round(2) if queued_ms
 
+    LogrageRailsRequestQueuing::ExceptionDetails.add_any_exception!(
+      event, custom_options
+    )
+
     custom_options
   end
+
+  ActionDispatch::DebugExceptions.prepend(
+    LogrageRailsRequestQueuing::SilenceExceptionLogging
+  )
 end
