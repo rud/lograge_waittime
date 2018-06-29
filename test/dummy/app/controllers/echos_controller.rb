50a2b4648e2f5a6af97fe023fc6a7cdcad195d75
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-29 20:38:29 +0200

    Dummy app: allow raising exceptions for an easy demo

--- test/dummy/app/controllers/echos_controller.rb
+++ test/dummy/app/controllers/echos_controller.rb
@@ -1,5 +1,6 @@
 class EchosController < ApplicationController
   def index
+    raise ArgumentError, 'Insufficient mittens' if params[:demo_raise]
     render json: {
       request_queued_raw: RequestStore[:lograge_request_queueing].request_queued_raw,
       queued_ms: RequestStore[:lograge_request_queueing].queued_ms
