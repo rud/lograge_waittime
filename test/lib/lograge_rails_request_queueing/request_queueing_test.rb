d9da0eadc2c84f5f0bb4de1bca7d036aeb036de0
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-29 14:33:24 +0200

    RequestQueueing: support timestamps given in ms instead of seconds

--- test/lib/lograge_rails_request_queueing/request_queueing_test.rb
+++ test/lib/lograge_rails_request_queueing/request_queueing_test.rb
@@ -15,6 +15,29 @@ module LogrageRailsRequestQueuing
         calculator.request_queued_raw,
       )
       assert_equal(
+        '2018-06-21T13:03:17+02:00',
+        calculator.request_queued_at.iso8601
+      )
+      assert_equal(
+        1222.33,
+        calculator.queued_ms.round(2)
+      )
+    end
+
+    test 'parsing a heroku request started header' do
+      calculator = LogrageRailsRequestQueuing::RequestQueueing.new(
+        { 'HTTP_X_REQUEST_START' => '1529578997111' },
+        fake_now = 1529578998.33333
+      )
+      assert_equal(
+        '1529578997111',
+        calculator.request_queued_raw,
+      )
+      assert_equal(
+        '2018-06-21T13:03:17+02:00',
+        calculator.request_queued_at.iso8601
+      )
+      assert_equal(
         1222.33,
         calculator.queued_ms.round(2)
       )
