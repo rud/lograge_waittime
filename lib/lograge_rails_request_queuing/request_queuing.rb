a240165a374397b668b86918bc01459aea4248cb
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-29 13:20:07 +0200

    RequestQueuing: make request_queued_raw value available for inspection

--- lib/lograge_rails_request_queuing/request_queuing.rb
+++ lib/lograge_rails_request_queuing/request_queuing.rb
@@ -4,11 +4,13 @@ module LogrageRailsRequestQueuing
   class RequestQueueing
     # How long was the request queued for, in milliseconds
     attr_reader :queued_ms
+    attr_reader :request_queued_raw
 
     REQUEST_START_HEADER = 'HTTP_X_REQUEST_START'
 
     def initialize(env, request_started_at = Time.now.to_f)
-      request_received_at = incoming_timestamp(env)
+      @request_queued_raw = request_start_header(env)
+      request_received_at = incoming_timestamp
       @queued_ms = calculate_queued_ms(request_received_at, request_started_at)
     end
 
@@ -23,12 +25,15 @@ module LogrageRailsRequestQueuing
       waiting_interval_secs * 1000
     end
 
-    def incoming_timestamp(env)
-      request_start = env[REQUEST_START_HEADER]
-      return nil if request_start.blank?
+    def incoming_timestamp
+      return nil if request_queued_raw.blank?
 
        # convert "t=1529578997.145" to a Float:
-      Float(Regexp.last_match(1)) if request_start =~ /t=([.\d+]+)/
+      Float(Regexp.last_match(1)) if request_queued_raw =~ /t=([.\d+]+)/
+    end
+
+    def request_start_header(env)
+      env[REQUEST_START_HEADER]
     end
   end
 end
