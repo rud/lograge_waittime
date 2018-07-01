f450fe7ea911759adf4b3ae9a11a8bc11e43f48f
Laust Rud Jacobsen <laust@valuestream.io>
2018-07-01 16:16:06 +0200

    CI: skip loading Rails defaults as it varies by version

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -19,7 +19,7 @@ require "lograge_rails_request_queuing"
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
