90d3c8bfeb9eb63475e0314e23326effc53a6975
Laust Rud Jacobsen <lrj@bdq.dk>
2022-11-18 16:02:44 +0100

    Engine: ensure we are inserted after RequestStore does its thing

--- lib/lograge_waittime/engine.rb
+++ lib/lograge_waittime/engine.rb
@@ -6,7 +6,8 @@ module LogrageWaittime
     config.autoload_once_paths << File.expand_path("lib", __dir__)
 
     initializer "lograge_waittime.add_waittime_middleware" do |app|
-      app.middleware.use LogrageWaittime::RequestStartedMiddleware
+      # As we use RequestStore, setup our middleware after that one
+      app.middleware.insert_after RequestStore::Middleware, LogrageWaittime::RequestStartedMiddleware
     end
   end
 end
